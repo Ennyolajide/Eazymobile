0f2e6ef2c7dc23e25ac51c1fdccc7a2e8704b29c
Ennyolajide <eniseyinolajide@gmail.com>
2020-03-06 17:47:09 +0100

    updated AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') !== 'local') {
+        if (config('constants.site.env') == 'production') {
             URL::forceScheme('https');
         }
 
