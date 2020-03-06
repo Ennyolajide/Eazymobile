61c44f89f01bdb522ab005897a577949d3736847
Ennyolajide <eniseyinolajide@gmail.com>
2020-03-06 17:09:33 +0100

    set public path to this directory 3

--- public/index.php
+++ public/index.php
@@ -42,7 +42,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 // set the public path to this directory
 $app->bind('path.public', function() {
-    return __DIR__.'/../';
+    return __DIR__.'/../public';
 });
 
 
