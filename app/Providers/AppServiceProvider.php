52fbf6f11181bbc7b05f4d61b2dc1fc7b4152ae7
Ennyolajide <eniseyinolajide@gmail.com>
2020-03-06 17:22:49 +0100

    set public path to this directory 5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,8 +32,9 @@ class AppServiceProvider extends ServiceProvider
     {
 
         $this->app->bind('path.public', function() {
-            return base_path('public');
+            return base_path().'/public';
         });
+
 
     }
 }
